2c80cace8226e3f183d67c6d324e3b6fa58debef
redebron20 <redebron20@gmail.com>
2021-05-22 10:09:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTaskForm = document.getElementById('create-task-form');
+
+  const newTaskUL = document.getElementById('tasks');
+
+  newTaskForm.addEventListener('submit', createNewTask);  
 });
+
+function createNewTask(event) {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTask = document.createElement('li');
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+function appendNewTask(task) {
+  document.getElementById("tasks").appendChild(task);
+};
